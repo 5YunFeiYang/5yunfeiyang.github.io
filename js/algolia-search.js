ddb79ee723414da9214f8d00bf574db4cdce5ebf
yangyunfei <yangyunfei@gaoxing>
2019-07-05 16:12:18 +0800

    Site updated: 2019-07-05 16:12:18

--- js/algolia-search.js
+++ js/algolia-search.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   var algoliaSettings = CONFIG.algolia;
-  var isAlgoliaSettingsValid = algoliaSettings.applicationID
+  var isAlgoliaSettingsValid = algoliaSettings.appID
                             && algoliaSettings.apiKey
                             && algoliaSettings.indexName;
 
@@ -12,7 +12,7 @@ $(document).ready(function() {
   }
 
   var search = instantsearch({
-    appId         : algoliaSettings.applicationID,
+    appId         : algoliaSettings.appID,
     apiKey        : algoliaSettings.apiKey,
     indexName     : algoliaSettings.indexName,
     searchFunction: function(helper) {
